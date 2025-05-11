72f65f4cb47236e08dd80b4cb178aacb053b32ac
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-05-11 14:21:17 +0200

    vyřešeno

--- src/components/Ukol1/index.jsx
+++ src/components/Ukol1/index.jsx
@@ -12,7 +12,7 @@ export const Ukol1 = () => {
     <>
       <label>
         Login:
-        <input type="text" />
+        <input type="text" value={login} onChange={(e) => setLogin(e.target.value)}/>
       </label>
     </>
   );
